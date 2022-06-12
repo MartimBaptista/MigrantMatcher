100c7e395fe09c699030830dbd749ae33c738b8b
Martim Baptista <martimdsbaptista@gmail.com>
2022-06-12 15:24:30 +0100

    Formating fix

--- src/Catalogs/MigranteCatalog.java
+++ src/Catalogs/MigranteCatalog.java
@@ -8,24 +8,23 @@ import Users.Migrante;
 public class MigranteCatalog {
 	private static MigranteCatalog instance;
 	private Map<Integer, Migrante> catalog;
-	
+
 	private MigranteCatalog() {
 		catalog = new HashMap<>();
 	}
-	
-	public static MigranteCatalog getInstance(){
-		if(instance == null){
+
+	public static MigranteCatalog getInstance() {
+		if (instance == null) {
 			return new MigranteCatalog();
-		}
-		else {
+		} else {
 			return instance;
 		}
 	}
-	
-	public void registarMigrante(int tele, Migrante migrante){
+
+	public void registarMigrante(int tele, Migrante migrante) {
 		catalog.put(tele, migrante);
 	}
-	
+
 	public Migrante obterMigrante(int tele) {
 		return catalog.get(tele);
 	}
