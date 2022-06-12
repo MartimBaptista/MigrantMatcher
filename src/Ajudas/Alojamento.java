3884a72797da29692cc43e1124ac27ac14e2fb36
Martim Baptista <martimdsbaptista@gmail.com>
2022-06-12 20:11:08 +0100

    Changed english var name to portuguese (for consistency)

--- src/Ajudas/Alojamento.java
+++ src/Ajudas/Alojamento.java
@@ -3,17 +3,17 @@ package Ajudas;
 import Users.*;
 
 public class Alojamento extends Ajuda {
-	private int capacity;
+	private int capacidade;
 	private String regiao;
 	
 	public Alojamento(Migrante migrante, Voluntario voluntario, int capacity, String regiao) {
 		super(migrante, voluntario);
-		this.capacity = capacity;
+		this.capacidade = capacity;
 		this.regiao = regiao;
 	}
 	
 	int getCapacity() {
-		return this.capacity;
+		return this.capacidade;
 	}
 	
 	String getregiao() {
