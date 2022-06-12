13251a323b195be35a2d146e8a66043a1c5d87bc
Martim Baptista <martimdsbaptista@gmail.com>
2022-06-12 15:26:52 +0100

    VoluntarioCatalog implemented

--- src/Catalogs/VoluntarioCatalog.java
+++ src/Catalogs/VoluntarioCatalog.java
@@ -1,5 +1,31 @@
 package Catalogs;
 
-public class VoluntarioCatalog {
+import java.util.HashMap;
+import java.util.Map;
 
+import Users.Voluntario;
+
+public class VoluntarioCatalog {
+	private static VoluntarioCatalog instance;
+	private Map<Integer, Voluntario> catalog;
+
+	private VoluntarioCatalog() {
+		catalog = new HashMap<>();
+	}
+
+	public static VoluntarioCatalog getInstance() {
+		if (instance == null) {
+			return new VoluntarioCatalog();
+		} else {
+			return instance;
+		}
+	}
+
+	public void registarVoluntario(int tele, Voluntario voluntario) {
+		catalog.put(tele, voluntario);
+	}
+
+	public Voluntario obterVoluntario(int tele) {
+		return catalog.get(tele);
+	}
 }
