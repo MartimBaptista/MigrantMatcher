5a01addcea28f38ff10025479c913bbdd2b647f6
Martim Baptista <martimdsbaptista@gmail.com>
2022-06-12 15:52:50 +0100

    Early attempt at implementing "RegistaAjudaHendler" (all commented out)

--- src/Handlers/RegistaAjudaHandler.java
+++ src/Handlers/RegistaAjudaHandler.java
@@ -3,28 +3,25 @@ import Ajudas.*;
 
 public class RegistaAjudaHandler {
 	
-	private Ajuda ajuda;
-	
-	public void indicaVoluntario(int tele) {
-		//criar voluntario
-		
-	}
-	//para Ajuda do tipo alojamento
-	public void indicaNumPessoas(int num) {
-		
-	}
-	
-	public void indicaRegiao(String regiao) {
-		
-	}
-	//para Ajuda do tipo Item
-	public void descreveItem(String desc) {
-		this.ajuda = new Item(desc);
-		
-	}
-
-	public void confirmarCodigo(int codUser) {
-	
-	}
+	/*
+	 * private Ajuda ajuda;
+	 * 
+	 * public void indicaVoluntario(int tele) { //criar voluntario
+	 * 
+	 * } //para Ajuda do tipo alojamento public void indicaNumPessoas(int num) {
+	 * 
+	 * }
+	 * 
+	 * public void indicaRegiao(String regiao) {
+	 * 
+	 * } //para Ajuda do tipo Item public void descreveItem(String desc) {
+	 * this.ajuda = new Item(desc);
+	 * 
+	 * }
+	 * 
+	 * public void confirmarCodigo(int codUser) {
+	 * 
+	 * }
+	 */
 	
 }
