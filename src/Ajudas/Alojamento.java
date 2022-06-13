c5c23dcd3d884568bae5eb79fafa0a13a7c0bdb6
Martim Baptista <martimdsbaptista@gmail.com>
2022-06-13 12:46:23 +0100

    Added a list of usable regions

--- src/Ajudas/Alojamento.java
+++ src/Ajudas/Alojamento.java
@@ -5,6 +5,29 @@ import Users.*;
 public class Alojamento extends Ajuda {
 	private int capacidade;
 	private String regiao;
+	private static final String[] regioes = {
+											"Alentejo Central",
+											"Alentejo Litoral",
+											"Alto Alentejo",
+											"Baixo Alentejo",
+											"Leziria do Tejo",
+											"Algarve",
+											"Beira Baixa",
+											"Beiras e Serra da Estrela",
+											"Medio Tejo",
+											"Aveiro",
+											"Coimbra",
+											"Leiria",
+											"Viseu",
+											"Lisboa",
+											"Alto Minho",
+											"Alto Tamega",
+											"Porto",
+											"Ave",
+											"Cavado",
+											"Douro",
+											"Tamega e Sousa",
+											"Tras-os-Montes"};
 	
 	public Alojamento(Migrante migrante, Voluntario voluntario, int capacity, String regiao) {
 		super(migrante, voluntario);
@@ -12,11 +35,16 @@ public class Alojamento extends Ajuda {
 		this.regiao = regiao;
 	}
 	
-	int getCapacity() {
+	public int getCapacity() {
 		return this.capacidade;
 	}
 	
-	String getregiao() {
+	public String getRegiao() {
 		return this.regiao;
 	}
+	
+	public String[] getRegioes() {
+		return regioes;
+	}
+	
 }
