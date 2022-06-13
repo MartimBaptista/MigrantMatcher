dd79f62021d5d84cff14770369c6d619b7215dfa
Martim Baptista <martimdsbaptista@gmail.com>
2022-06-13 13:31:35 +0100

    Region list moved from Alojamento to Ajuda

--- src/Ajudas/Ajuda.java
+++ src/Ajudas/Ajuda.java
@@ -6,6 +6,11 @@ public abstract class Ajuda {
 	protected Migrante migrante;
 	protected Voluntario voluntario;
 
+	private static final String[] regioes = { "Alentejo Central", "Alentejo Litoral", "Alto Alentejo", "Baixo Alentejo",
+			"Leziria do Tejo", "Algarve", "Beira Baixa", "Beiras e Serra da Estrela", "Medio Tejo", "Aveiro", "Coimbra",
+			"Leiria", "Viseu", "Lisboa", "Alto Minho", "Alto Tamega", "Porto", "Ave", "Cavado", "Douro",
+			"Tamega e Sousa", "Tras-os-Montes" };
+
 	protected Ajuda(Migrante migrante, Voluntario voluntario) {
 		this.migrante = migrante;
 		this.voluntario = voluntario;
@@ -17,5 +22,9 @@ public abstract class Ajuda {
 
 	public Voluntario getvoluntario() {
 		return voluntario;
+	}
+
+	public String[] getRegioes() {
+		return regioes;
 	}
 }
--- src/Ajudas/Alojamento.java
+++ src/Ajudas/Alojamento.java
@@ -5,46 +5,18 @@ import Users.*;
 public class Alojamento extends Ajuda {
 	private int capacidade;
 	private String regiao;
-	private static final String[] regioes = {
-											"Alentejo Central",
-											"Alentejo Litoral",
-											"Alto Alentejo",
-											"Baixo Alentejo",
-											"Leziria do Tejo",
-											"Algarve",
-											"Beira Baixa",
-											"Beiras e Serra da Estrela",
-											"Medio Tejo",
-											"Aveiro",
-											"Coimbra",
-											"Leiria",
-											"Viseu",
-											"Lisboa",
-											"Alto Minho",
-											"Alto Tamega",
-											"Porto",
-											"Ave",
-											"Cavado",
-											"Douro",
-											"Tamega e Sousa",
-											"Tras-os-Montes"};
-	
+
 	public Alojamento(Migrante migrante, Voluntario voluntario, int capacity, String regiao) {
 		super(migrante, voluntario);
 		this.capacidade = capacity;
 		this.regiao = regiao;
 	}
-	
+
 	public int getCapacity() {
 		return this.capacidade;
 	}
-	
+
 	public String getRegiao() {
 		return this.regiao;
 	}
-	
-	public String[] getRegioes() {
-		return regioes;
-	}
-	
 }
