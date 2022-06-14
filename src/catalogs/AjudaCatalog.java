1d1461deeb7ead25365c110cec9eadb44491d8bb
Martim Baptista <martimdsbaptista@gmail.com>
2022-06-14 18:45:16 +0100

    Removed getSorted function

--- src/catalogs/AjudaCatalog.java
+++ src/catalogs/AjudaCatalog.java
@@ -1,23 +1,18 @@
 package catalogs;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
 import ajudas.Ajuda;
-import sorters.Sorter;
-import sorters.sortByDate;
-import config.Configuration;
+
 
 public class AjudaCatalog implements CatalogInterface<Ajuda> {
 	private static AjudaCatalog instance = new AjudaCatalog();
-	private Sorter sorter;
 	private Map<String, Ajuda> catalog;
 
 	private AjudaCatalog() {
 		catalog = new HashMap<>();
-		sorter = Configuration.getInstance().getInstanceOfClass("sortType", new sortByDate());
 	}
 
 	public static AjudaCatalog getInstance() {
@@ -37,9 +32,5 @@ public class AjudaCatalog implements CatalogInterface<Ajuda> {
 	@Override
 	public Iterator<Ajuda> getIterator() {
 		return catalog.values().iterator();
-	}
-	
-	public Collection<Ajuda> getSortedAjudas(){
-		return sorter.sort(catalog.values());
 	}
 }
