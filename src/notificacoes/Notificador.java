0b5b7109fa8adc15b83c80adc3271bc9d161f976
Martim Baptista <martimdsbaptista@gmail.com>
2022-06-14 17:55:10 +0100

    Notificador implemented

--- src/notificacoes/Notificador.java
+++ src/notificacoes/Notificador.java
@@ -3,17 +3,26 @@ package notificacoes;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
 
+import config.Configuration;
+import sms.PidgeonSMSAdapter;
+import sms.SMSProvider;
 import users.Migrante;
 
 
 public class Notificador {
 	private static Notificador instance = new Notificador();
 	private Map<String, Collection<Migrante>> subscribers;
+	private SMSProvider provider = Configuration.getInstance().getInstanceOfClass("smsProvider", new PidgeonSMSAdapter());
+	
 
 	private Notificador() {
 		subscribers = new HashMap<>();
-		//TODO
+		String[] regioes = Configuration.getInstance().getStringArray("regioes");
+		for (String string : regioes) {
+			subscribers.put(string, new ArrayList<Migrante>());
+		}
 	}
 
 	public static Notificador getInstance() {
@@ -22,14 +31,17 @@ public class Notificador {
 	
 	
 	public void subscribe(Migrante migrante, String regiao) {
-		
+		subscribers.get(regiao).add(migrante);
 	}
 	
 	public void unsubscribe(Migrante migrante, String regiao) {
-		
+		subscribers.get(regiao).remove(migrante);
 	}
 	
 	public void notificar(String regiao) {
-		
+		ArrayList<Migrante> toNotify = (ArrayList<Migrante>) subscribers.get(regiao);
+		for (Migrante migrante : toNotify) {
+			provider.sendSMS(migrante.getTele(), ("A região " + regiao + " tem ajudas disponiveis."));
+		}
 	}
 }
