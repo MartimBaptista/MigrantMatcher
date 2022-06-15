dd52282a994698906dd6a5e14b243f9743a8f79b
Joao Serafim 56376 <fc56376@alunos.fc.ul.pt>
2022-06-15 18:15:42 +0100

    Exception case 2 fix.

--- src/ui/PedeAjudaUI.java
+++ src/ui/PedeAjudaUI.java
@@ -33,13 +33,13 @@ public class PedeAjudaUI {
 				if(numPessoas > 0)
 					valid = true;
 				else
-					System.out.print("Valor não reconhecido. Reinsira: ");
+					System.out.print("Valor nï¿½o reconhecido. Reinsira: ");
 			}
 			pedeAjudaHandler.indicaNumFamilia(numPessoas);
 			// 1.2.(1,2)
-			System.out.print("Insira o nome do cabeça de familia: ");
+			System.out.print("Insira o nome do cabeï¿½a de familia: ");
 			pedeAjudaHandler.indicaNomeMigrante(sc.nextLine()); //Bug here
-			System.out.print("Insira o numero de telefone do cabeça de familia: ");
+			System.out.print("Insira o numero de telefone do cabeï¿½a de familia: ");
 			pedeAjudaHandler.indicaTeleMigrante(sc.nextLine());
 			// 1.2.(3,4,5)
 			System.out.println("Indique agora os nomes de outros familiares.");
@@ -51,26 +51,27 @@ public class PedeAjudaUI {
 		pedeAjudaHandler.registarMigrante();
 		
 		// 2,3,4
-		System.out.println("Regiões:");
+		System.out.println("Regiï¿½es:");
 		for (int i = 0; i < REGIOES.length; i++) {
 			System.out.println((i + 1) + ": " + REGIOES[i]);
 		}
 		boolean stillSelecting = true;
 		int index = -1;
 		while (stillSelecting) {
-			System.out.print("Insira o numero da região para onde se quer mover: ");
+			System.out.print("Insira o numero da regiï¿½o para onde se quer mover: ");
 			index = Integer.valueOf(sc.nextLine()) - 1;
 			if(index >= 0 && index < REGIOES.length)
 				stillSelecting = false;
 			else 
-				System.out.println("Valor não reconhecido.");
+				System.out.println("Valor nï¿½o reconhecido.");
 		}
 		pedeAjudaHandler.registaRegiao(REGIOES[index]);
 		
 		// 5
 		boolean repeat = true;
 		while (repeat) {
-			Ajuda[] ajudas = (Ajuda[]) pedeAjudaHandler.ajudasDisponiveis().toArray();
+			int tam = pedeAjudaHandler.ajudasDisponiveis().size();
+			Ajuda[] ajudas = pedeAjudaHandler.ajudasDisponiveis().toArray(new Ajuda[tam]);
 			if (ajudas.length > 0) {
 				for (int i = 0; i < ajudas.length; i++) {
 					System.out.println((i + 1) + ": " + ajudas[i]);
@@ -84,21 +85,21 @@ public class PedeAjudaUI {
 					if (index >= 0 && index < ajudas.length)
 						stillSelecting = false;
 					else
-						System.out.println("Valor não reconhecido.");
+						System.out.println("Valor nï¿½o reconhecido.");
 				}
 				// 7,10
 				pedeAjudaHandler.indicaAjuda(ajudas[index]);
 				
 				// 8,9
-				System.out.print("Obter mais ajudas? ('S' Sim, 'N' Não): ");
+				System.out.print("Obter mais ajudas? ('S' Sim, 'N' Nï¿½o): ");
 				if(sc.nextLine().toUpperCase() == "N")
 					repeat = false;
 			}
 			// 5a
 			else {
 				//5a.5
-				System.out.println("Não existe nenhuma ajuda nesta região :(");
-				System.out.print("Deseja ser notificado quando houver? ('S' Sim, 'N' Não): ");
+				System.out.println("Nï¿½o existe nenhuma ajuda nesta regiï¿½o :(");
+				System.out.print("Deseja ser notificado quando houver? ('S' Sim, 'N' Nï¿½o): ");
 				if(sc.nextLine().toUpperCase() == "S")
 					pedeAjudaHandler.ativarNotificacao();
 			}
